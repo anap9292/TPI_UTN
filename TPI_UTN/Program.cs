8d7399d9c9f7ddb4c9699adbd6991e9cd77821d4
anap9292 <anahiandrea.p@gmail.com>
2022-12-19 12:23:35 -0300

    Modifiqué lo referente a Usuario, agregué el login

--- TPI_UTN/Program.cs
+++ TPI_UTN/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
